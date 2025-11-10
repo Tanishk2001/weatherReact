e28d0248d01d6364c0aaaca9b0b9b1d47b22c421
tanishk agrawal <tanishk15112001@gmail.com>
2025-11-10 14:11:45 +0530

    Add API keys for weather and location services

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
+    "X-RapidAPI-Key": "3a6f2c36a1msh61f1e9d4b050de7p154cbbjsncd428700b9ba",
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "f477e7a11b06f6204c5192ad6482ad76";
